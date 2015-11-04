f2536ac4419ede8be675db65958c673512f2e8e7
Ceybb <Ceybb@users.noreply.github.com>
2015-11-04 21:49:02 +0100

    Create CapteursRoutiers.h

---
+++ CapteursRoutiers.h
@@ -0,0 +1,89 @@
+/*************************************************************************
+                           CapteursRoutiers  -  description
+                             -------------------
+    début                : 21/10/2015
+    copyright            : (C) 2015 par abarzasi sdigiovann
+*************************************************************************/
+
+//---------- Interface de la classe <CapteursRoutiers> (fichier CapteursRoutiers.h) ------
+#if ! defined ( CAPTEURSROUTIERS_H )
+#define CAPTEURSROUTIERS_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <CapteursRoutiers>
+//
+//
+//------------------------------------------------------------------------
+
+class CapteursRoutiers //: public Ancetre
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste de paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    CapteursRoutiers & operator = ( const CapteursRoutiers & unCapteursRoutiers );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    CapteursRoutiers ( const CapteursRoutiers & unCapteursRoutiers );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    CapteursRoutiers ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~CapteursRoutiers ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+    char tabValeurs[7][5][31][24][60][1500];
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <CapteursRoutiers>
+
+#endif // CAPTEURSROUTIERS_H
