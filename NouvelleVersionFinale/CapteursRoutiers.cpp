9fd55a540ad21caba1dc4115843ad0aeede3825c
Slouby <alan.barzasi@gmail.com>
2015-11-09 22:32:09 +0100

    Update CapteursRoutiers.cpp

--- NouvelleVersionFinale/CapteursRoutiers.cpp
+++ NouvelleVersionFinale/CapteursRoutiers.cpp
@@ -25,6 +25,12 @@ void TestsTabEtatConstructeur1 ()
 	TabEtats unTabEtats;
 }
 
+void TestsTabEtatAjouter1()
+{
+	TabEtats unTabEtats;
+	unTabEtats.Ajouter(2,24,5,21,3,'V');
+}
+
 void TestsIndexConstructeur1 ()
 {
 	Index unIndex;
@@ -65,11 +71,32 @@ void TestsIndexAjouterId3 ()
 	unIndex.Afficher();
 }
 
+void TestsIndexTestId1()
+{
+	Index unIndex;
+	cout << unIndex.TestId(322) << endl;
+	unIndex.AjouterId(322);
+	cout << unIndex.TestId(322) << endl;
+	unIndex.AjouterId(144);
+	unIndex.AjouterId(555);
+	cout << unIndex.TestId(555) << endl;
+	unIndex.Afficher();
+}
 
+void TestsLecture1()
+{
+	Index unIndex;
+	unIndex.Lecture(122,2,31,12,48,1,'V');
+}
 
 void TestsTabEtatConstructeur ()
 {
 	TestsTabEtatConstructeur1();
+}
+
+void TestsTabEtatAjouter()
+{
+	TestsTabEtatAjouter1();
 }
 
 void TestsIndexConstructeur ()
@@ -84,25 +111,35 @@ void TestsIndexAjouterId ()
 	TestsIndexAjouterId3();
 }
 
-
+void TestsIndexTestId()
+{
+	TestsIndexTestId1();
+}
 
 void TestsTabEtats ()
 {
 	TestsTabEtatConstructeur();
+	TestsTabEtatAjouter();
 }
 
 void TestsIndex ()
 {
 	TestsIndexConstructeur();
 	TestsIndexAjouterId();
+	TestsIndexTestId();
 }
 
+void TestsLecture ()
+{
+	TestsLecture1();
+}
 
 
 void Tests ()
 {
 	TestsTabEtats();
 	TestsIndex();
+	TestsLecture();
 }
 
 
@@ -112,5 +149,53 @@ int main ()
 	cout << "Debut des tests" << endl;
 	Tests();
 	cout << "Fin des tests" << endl;
+
+	string commande;
+	int idCapteur;
+	int annee;
+	int mois;
+	int jour;
+	int heure;
+	int min;
+	int jourSemaine;
+	char trafic;
+	Index unIndex;
+
+	while (cin>>commande != "EXIT")
+	 {
+	    if (commande == "ADD")
+	    {	// Exécuter la méthode lecture
+			cout << " Boucle lecture " << endl;
+			cin >> idCapteur >> annee >> mois >> jour >> heure >> min >> jourSemaine >> trafic;
+			unIndex.Lecture(idCapteur, mois, jour, heure, min, jourSemaine, trafic);
+			cout << idCapteur << " / " << annee << " / " << mois << " / " << jour << " / " << heure <<" / " << min << " / " << jourSemaine << " / " << trafic << endl;
+		}
+	    else if (commande == "STATS_C")
+	    {	//Exécuter la méthode StatsCapteur
+	    	cout << " Boucle StatsCapteur " << endl;
+	    	cin >> idCapteur;
+	    	cout << idCapteur << endl;
+	    }
+	    else if (commande == "JAM_DH")
+	    {	// Exécuter la méthode Embouteillage
+	    	cout << " Boucle Embouteillage " << endl;
+	    	cin >> jourSemaine;
+	    	cout << jourSemaine << endl;
+	    }
+	    else if (commande == "STATS_D7")
+		{	//Exécuter la méthode StatsJour
+			cout << " Boucle StatsJour " << endl;
+			cin >> jourSemaine;
+			cout << jourSemaine << endl;
+
+	    }
+	    else if (commande == "OPT")
+		{	//Exécuter la méthode Optimisation
+			cout << " Boucle Optimisation " << endl;
+	    	/* cin >> ?; A faire
+			cout << ? << endl;*/
+		}
+		cin >> commande;
+	  }
 	return 0;
 }
