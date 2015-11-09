afa0828958db8fb7ebbaf17d3f98e5a5e2b93d5e
Slouby <alan.barzasi@gmail.com>
2015-11-09 14:00:51 +0100

    Update TabEtats.cpp

--- NouvelleVersionFinale/TabEtats.cpp
+++ NouvelleVersionFinale/TabEtats.cpp
@@ -1,12 +1,12 @@
 /*************************************************************************
-                           TabEtats  -  description
+                           Index  -  description
                              -------------------
     début                : 21/10/2015
     copyright            : (C) 2015 par abarzasi sdigiovann
 *************************************************************************/
 
-//---------- Réalisation de la classe <TabEtats> (fichier TabEtats.cpp) --
-
+//---------- Réalisation de la classe <Index> (fichier Index.cpp) --
+#define MAP
 //---------------------------------------------------------------- INCLUDE
 
 //-------------------------------------------------------- Include système
@@ -14,6 +14,7 @@ using namespace std;
 #include <iostream>
 
 //------------------------------------------------------ Include personnel
+#include "Index.h"
 #include "TabEtats.h"
 
 //------------------------------------------------------------- Constantes
@@ -27,51 +28,84 @@ using namespace std;
 //-------------------------------------------------------- Fonctions amies
 
 //----------------------------------------------------- Méthodes publiques
-// type TabEtats::Méthode ( liste de paramètres )
-// Algorithme :
-//
-//{
-//} //----- Fin de Méthode
-
-void TabEtats::Ajouter (int mois, int jour, int heure, int minute, int jourSemaine, char trafic)
+codeRetour Index::AjouterId (int unId)
 {
-	int numJour=0;
-	if(8<=jour && jour<=14)
-	{numJour=1;
+	codeRetour unCodeRetour=nbCapteursMaxNonAjuste;
+	if (nbCapteursCourant==nbCapteursMax)
+	{	Ajuster();
+		unCodeRetour=nbCapteursMaxAjuste;
 	}
-	else if(15<=jour && jour<=21)
-		{numJour=2;
-		}
-	else if(22<=jour && jour<=28)
-		{numJour=3;
-		}
-	else if(29<=jour && jour<=31)
-		{numJour=4;
-		}
-	tabEtats[jourSemaine][mois][numJour][heure][minute]=trafic;
-} //} //----- Fin de Méthode
+	id[nbCapteursCourant]=unId;
+	nbCapteursCourant++;
+	return unCodeRetour;
+} //----- Fin de Méthode
 
-
-//-------------------------------------------- Constructeurs - destructeur
-TabEtats::TabEtats ( )
+void Index::Afficher () const
 {
-#ifdef MAP
-    cout << "Appel au constructeur de <TabEtats>" << endl;
-#endif
-for (int jourSem=0;jourSem<7;jourSem++)
-{	for (int mois=0;mois<5;mois++)
-	{	for (int numJour=0;numJour<5;numJour++)
-		{	for (int heure=0;heure<24;heure++)
-			{	for (int minute=0;minute<60;minute++)
-				{	tabEtats[jourSem][mois][numJour][heure][minute]='A';
-				}
-			}
-		}
+	cout << nbCapteursCourant << "/" << nbCapteursMax << endl;
+	for (int i=0;i<nbCapteursCourant;i++)
+	{	cout << id[i] << endl;
 	}
 }
-} //----- Fin de TabEtats
+
+/*codeRetour Index::AjouterDate (int mois, int jour, int heure, int minute, int jourSemaine)
+{
+	codeRetour unCodeRetour=nbCapteursMaxNonAjuste;
+	if (nbCapteursCourant==nbCapteursMax)
+	{	Ajuster();
+		unCodeRetour=nbCapteursMaxAjuste;
+	}
+	index[nbCapteursCourant];
+	nbCapteursCourant++;
+	return unCodeRetour;
+}*/
+
+int Index::TestId (int unId)
+{
+	int idExiste = -1;
+	for (int i=0;i<nbCapteursCourant;i++)
+	{	if(id[i]==unId)
+		{idExiste=i;
+		break;
+		}
+	}
+	return idExiste;
+} //----- Fin de Méthode
+
+void Index::Lecture(int idCapteur, int mois, int jour, int heure, int minute, int jourSemaine, char trafic)
+{
+	int testId=TestId(idCapteur);
+	if(testId==-1)
+	{
+		AjouterId(idCapteur);
+		(*(index[nbCapteursCourant-1])).Ajouter(mois,jour,heure,minute,jourSemaine,trafic);
+	}
+	else
+	{
+		(*(index[testId])).Ajouter(mois,jour,heure,minute,jourSemaine,trafic);
+	}
+}
+//-------------------------------------------- Constructeurs - destructeur
+Index::Index ()
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Index>" << endl;
+#endif
+    nbCapteursCourant=0;
+    nbCapteursMax=10;
+    id=new int[nbCapteursMax];
+    index=new TabEtats*[nbCapteursMax];
+} //----- Fin de Index
 
 
+Index::~Index ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Index>" << endl;
+#endif
+    delete [] id;
+    delete [] index;
+} //----- Fin de ~Index
 
 
 //------------------------------------------------------------------ PRIVE
@@ -79,3 +113,18 @@ for (int jourSem=0;jourSem<7;jourSem++)
 //----------------------------------------------------- Méthodes protégées
 
 //------------------------------------------------------- Méthodes privées
+void Index::Ajuster ()
+{
+	nbCapteursMax=1.5*nbCapteursMax;
+	int * nouvTabId = new int [nbCapteursMax];
+	TabEtats ** nouvIndex = new TabEtats*[nbCapteursMax];
+	for (int i=0;i<nbCapteursCourant;i++)
+	{	nouvTabId[i]=id[i];
+		nouvIndex[i]=index[i];
+	}
+	delete [] id;
+	delete [] index;
+	id=nouvTabId;
+	index=nouvIndex;
+}
+
