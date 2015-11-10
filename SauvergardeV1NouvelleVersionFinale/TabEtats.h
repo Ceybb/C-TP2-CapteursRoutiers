9866cccd4cde0f7879bf1a2aadc32fd81a28ad3c
Ceybb <Ceybb@users.noreply.github.com>
2015-11-10 13:34:36 +0100

    Create TabEtats.h

---
+++ SauvergardeV1NouvelleVersionFinale/TabEtats.h
@@ -0,0 +1,87 @@
+/*************************************************************************
+                           TabEtats  -  description
+                             -------------------
+    début                : 21/10/2015
+    copyright            : (C) 2015 par abarzasi sdigiovann
+*************************************************************************/
+
+//---------- Interface de la classe <TabEtats> (fichier TabEtats.h) ------
+#if ! defined ( TABETATS_H )
+#define TABETATS_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TabEtats>
+//
+//
+//------------------------------------------------------------------------
+
+class TabEtats
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+	void Afficher () const;
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	void AjouterMesure (int mois, int jour, int heure, int minute, int jourSemaine, char trafic);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+	void StatsJSem (int jourSemaine,int &etatV, int &etatJ, int &etatR, int &etatN, int &nbMesures);
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+	void EmbouteillageJSemHeure (int jourSemaine,int * &tabEtatsHeures, int &nbMesures);
+	// Mode d'emploi :
+	//
+	// Contrat :
+	//
+
+//-------------------------------------------- Constructeurs - destructeur
+    TabEtats ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+    char tabEtats[7][5][5][24][60];
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <TabEtats>
+
+#endif // TABETATS_H
